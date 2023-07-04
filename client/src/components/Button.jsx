9170118bdb13917ffc763a485f942bb2edac503f
chrischristakis <chriscchristakis@gmail.com>
2023-07-04 16:28:41 -0400

    Confirm button

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ReactComponent as Loading } from '../assets/loading.svg';
 import style from './Button.module.css';
 
-function Button({text="placeholder", clickEvent=async ()=>{}, type='button', disabled=false}) {
+function Button({text="placeholder", clickEvent=async ()=>{}, type='button', disabled=false, shouldConfirm=false}) {
 
     const [loading , setLoading] = useState(false);
+    const [displayConfirm, setDisplayConfirm] = useState(false);
+
+    // After 5 seconds, if they havent confirmed, reset button.
+    useEffect(() => {
+        if(!displayConfirm) 
+            return;
+
+        setTimeout(() => {
+            if(displayConfirm) {
+                console.log('running!')
+                setDisplayConfirm(false);
+            }
+        }, 4000);
+    }, [displayConfirm, setDisplayConfirm])
 
     const handleClick = async (e) => {
+        if(shouldConfirm && !displayConfirm) {
+            setDisplayConfirm(true);
+            return;
+        }
+
         setLoading(true);
         try {
             //await new Promise(resolve => setTimeout(resolve, 5000)); For throttle testing
@@ -14,13 +33,24 @@ function Button({text="placeholder", clickEvent=async ()=>{}, type='button', dis
             setLoading(false);
         }
         catch(err) {
+            // This should be caught in our click event, however, we could move it here.
+        }
+        finally {
+            setDisplayConfirm(false);
             setLoading(false);
         }
     };
 
     return (
-        <button className={style.button} onClick={handleClick} type={type} disabled={loading || disabled}>
-            {loading? <Loading/> : text}
+        <button className={`${style.button} ${displayConfirm? style.confirm : style.normal}`} onClick={handleClick} type={type} disabled={loading || disabled}>
+            {loading? 
+                <Loading/> 
+                : 
+                displayConfirm ? 
+                    'you sure?'
+                    :
+                    text
+            }
         </button>
     );
 }
